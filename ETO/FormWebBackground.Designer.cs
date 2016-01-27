5ce83bc119ec93d26e8a3001d9050728f11ff507
Bill Hess <billdhess@hotmail.com>
2016-01-27 14:22:34 -0800

    daily

--- ETO/FormWebBackground.Designer.cs
+++ ETO/FormWebBackground.Designer.cs
@@ -30,32 +30,48 @@
         {
             this.webBackground = new System.Windows.Forms.WebBrowser();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // webBackground
             // 
             this.webBackground.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.webBackground.Location = new System.Drawing.Point(114, 1);
-            this.webBackground.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBackground.Location = new System.Drawing.Point(-10, 32);
+            this.webBackground.MinimumSize = new System.Drawing.Size(1, 1);
             this.webBackground.Name = "webBackground";
-            this.webBackground.Size = new System.Drawing.Size(49, 44);
+            this.webBackground.Size = new System.Drawing.Size(1, 1);
             this.webBackground.TabIndex = 0;
             this.webBackground.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBackground_DocumentCompleted);
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(27, 168);
+            this.listView1.CheckBoxes = true;
+            this.listView1.Location = new System.Drawing.Point(12, 0);
+            this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(727, 151);
+            this.listView1.Size = new System.Drawing.Size(727, 481);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(462, 492);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FormWebBackground
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(802, 493);
+            this.ClientSize = new System.Drawing.Size(759, 532);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.webBackground);
             this.Name = "FormWebBackground";
@@ -69,5 +85,6 @@
 
         public System.Windows.Forms.WebBrowser webBackground;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- ETO/FormWebBackground.cs
+++ ETO/FormWebBackground.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,21 +28,84 @@ namespace ETO
 
         private void FormWebBackground_Load(object sender, EventArgs e)
         {
-            listView1.View = View.Details;
-            listView1.Columns.Add("Name", 200, HorizontalAlignment.Left);
-            listView1.Columns.Add("SSN", 100, HorizontalAlignment.Left);
-            listView1.Columns.Add("CaseNumber", 100, HorizontalAlignment.Left);
-            listView1.Columns.Add("Participant Note", 240, HorizontalAlignment.Left);
+            
         }
 
         private void webBackground_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
+            string sURL = e.Url.ToString();
+            if (sURL.StartsWith("https://secure.etosoftware.com/SearchForClientResultExpanded.asp"))
+            {
+                listView1.View = View.Details;
 
+                listView1.Columns.Add("Name", 200, HorizontalAlignment.Left);
+                listView1.Columns.Add("SSN", 100, HorizontalAlignment.Left);
+                listView1.Columns.Add("CLID", 80, HorizontalAlignment.Left);
+                listView1.Columns.Add("CaseNumber", 100, HorizontalAlignment.Left);
+                listView1.Columns.Add("Participant Note", 240, HorizontalAlignment.Left);
+                string sCLID = "";
+                string sName = "";
+                string sSSN = "";
+                string sCaseNumber = "";
+                string sParticipantNote = "";
+                string sBody = webBackground.Document.Body.InnerHtml;
+                sBody = sBody.Substring(sBody.IndexOf("firstResult"));
+                sBody = sBody.Substring(sBody.IndexOf("'False','arrow','") + 17);
+                while (sBody != "")
+                    {
+                        sCLID = sBody.Substring(0, sBody.IndexOf("'"));
+                        sBody = sBody.Substring(sBody.IndexOf("','") + 3);
+                        sName = sBody.Substring(0, sBody.IndexOf("'"));
+                    //td3
+                    int iIndex =  sBody.IndexOf("td3");
+                    sBody = sBody.Substring(sBody.IndexOf("td3") + 4);
+                    sSSN = sBody.Substring(0, sBody.IndexOf("<"));
+                    //td3
+                    iIndex = sBody.IndexOf("td3\">");
+                    sBody = sBody.Substring(sBody.IndexOf("td3") + 4);
+                    sCaseNumber = sBody.Substring(0, sBody.IndexOf("<"));
+                    //td4
+                    iIndex = sBody.IndexOf("td3\">");
+                    sBody = sBody.Substring(sBody.IndexOf("td4") + 4);
+                    sParticipantNote = sBody.Substring(0, sBody.IndexOf("<"));
+                    var listViewItem = new ListViewItem(new[] {  sName,sSSN, sCLID, sCaseNumber, sParticipantNote});
+                    listView1.Items.Add(listViewItem);
+                    if (sBody.IndexOf("'False','arrow','")==-1)
+                    {
+                        sBody = "";
+                    }
+                    sBody = sBody.Substring(sBody.IndexOf("'False','arrow','") + 17);
+                }
+            }
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count != 0)
+            {
+                if (listView1.Items[0].Selected)
+                {
+                    
+                }
+                else
+                {
+                    string chosen = listView1.SelectedItems[0].Text;
+                    {
+
+                    }
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
